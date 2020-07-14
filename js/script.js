a0d586abc117492949f6a1d44ee889faad7028a5
mateooosh <mateusz20111@op.pl>
2020-07-14 19:03:36 +0200

    remove unnecessary stats

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 let countries = [];
-let confirmed = 0, deaths = 0, recovered = 0;
-let newConfirmed = 0, newDeaths = 0, newRecovered = 0;
-
 
 let xhr = new XMLHttpRequest();
-let xhr2 = new XMLHttpRequest();
 xhr.onload = function() {
 	if(xhr.status ===200){
 		responseObject = JSON.parse(xhr.responseText);
@@ -21,24 +17,15 @@ xhr.send(null);
 
 
 
-function prepare(){
-	sortowanie();
+let prepare = () => {
+	sort();
 	let tableRef = document.querySelector('tbody');
 	let temp ;
 	for(let i=0; i<countries.length; i++){
-		confirmed += countries[i].TotalConfirmed;
-		deaths += countries[i].TotalDeaths;
-		recovered += countries[i].TotalRecovered;
-		newConfirmed += countries[i].NewConfirmed;
-		newDeaths += countries[i].NewDeaths;
-		newRecovered += countries[i].NewRecovered;
 
 		// Insert a row at the end of the table
 		let newRow = tableRef.insertRow(-1);
 		newRow.insertCell(0).appendChild(document.createTextNode(countries[i].Country));
-
-		// temp = newRow.insertCell(0);
-		// $(temp).html(countries[i].flag+countries[i].Country);
 		
 		temp = newRow.insertCell(1);
 		$(temp).html(`${countries[i].TotalConfirmed} <span class='red'> (+${countries[i].NewConfirmed})</span>`);
@@ -49,16 +36,9 @@ function prepare(){
 		temp = newRow.insertCell(3);
 		$(temp).html(`${countries[i].TotalRecovered} <span class='green'> (+${countries[i].NewRecovered})</span>`);
 	}
-
-	$(".confirmed").html(confirmed);
-	$(".deaths").html(deaths);
-	$(".recovered").html(recovered);
-	$(".newConfirmed").html(`${newConfirmed} today`);
-	$(".newDeaths").html(`${newDeaths} today`);
-	$(".newRecovered").html(`${newRecovered} today`);
 }
 
-function sortowanie(){
+let sort = () => {
     for(let i=0; i<countries.length; i++){
         for(let j=countries.length-1; j>i; j--){
             if(countries[j].TotalConfirmed>countries[j-1].TotalConfirmed)
